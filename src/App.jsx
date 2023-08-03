68bf27dfb378fa30e651725aedb4c34a16a31199
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-03 13:00:39 -0700

    add states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Header from "./Components/Headers";
-import PatientList from "./Components/PatientList";
-import Form from "./Components/Form";
+import { useState } from 'react';
+
+import Header from './Components/Headers';
+import PatientList from './Components/PatientList';
+import PatientForm from './Components/PatientForm';
 
 function App() {
+  const [patients, setPatients] = useState([]);
+
   return (
-    <>
-      <Header />
-      <PatientList />
-      <Form />
-    </>
+    <div className="container mx-auto mt-20">
+      <Header numbers={1} />
+      <div className="mt-12 md:flex">
+        <PatientForm patients={patients} setPatients={setPatients} />
+        <PatientList />
+      </div>
+    </div>
   );
 }
 
